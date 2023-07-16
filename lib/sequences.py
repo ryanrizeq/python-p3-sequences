42e58f721db4e783138d562c84e2f97ab9843d1a
Ryan Rizeq <ryanrizeq@gmail.com>
2023-07-15 17:49:46 -0700

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = []
+    i = 0
+    j = 0
+    while i < length:
+        if i == 0:
+            sequence.append(0)
+        elif i == 1:
+            sequence.append(1)
+        else:
+            sequence.append(sequence[j] + sequence[j + 1])
+            j += 1
+        i += 1
+    print(sequence)
